7ffd35f7e4e33204dc567be193df1fb97963719c
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2025-11-30 16:18:42 -0500

    input pullup for pinmode

--- main_v2.ino
+++ main_v2.ino
@@ -1,32 +1,32 @@
 #include <Adafruit_NeoPixel.h>
 
-#define NEOPIXEL_PIN 3 // Data pin connected to the NeoPixel ring
-#define NUM_LEDS 16    // NeoPixel ring has 16 LEDs
+#define NEOPIXEL_PIN 3
+#define NUM_LEDS 16
+#define BTN_PIN 6
 
-#define BTN_PIN 6 // Data pin connected for button
 Adafruit_NeoPixel ring(NUM_LEDS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   ring.begin();
-  ring.show(); // Initialize all pixels to 'off'
-  pinMode(BTN_PIN, INPUT);    // sets the digital pin 7 as input
-
+  ring.show();
+  pinMode(BTN_PIN, INPUT_PULLUP); 
 }
 
 void loop() {
-  bool btn1 = digitalRead(BTN_PIN)
+  bool btn1 = digitalRead(BTN_PIN);  // HIGH = not pressed, LOW = pressed
 
-  if (!btn1) {
+  if (!btn1) {  // Button pressed
     for (int i = 0; i < NUM_LEDS; i++) {
-      ring.setPixelColor(i, ring.Color(255, 0, 0)); 
+      ring.setPixelColor(i, ring.Color(255, 0, 0));
     }
     ring.show();
-    delay(1000);
+  } else {
+    // Button not pressed → turn LEDs off
+    for (int i = 0; i < NUM_LEDS; i++) {
+      ring.setPixelColor(i, 0);
+    }
+    ring.show();
   }
-  
-  for (int i = 0; i < NUM_LEDS; i++) {
-    ring.setPixelColor(i, ring.Color(0, 0, 0));
-  }
-  ring.show();
-  delay(1000);
+
+  delay(20); // Small debounce
 }
