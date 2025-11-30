d245c3182ba25a33e66b202f3866f70d9a453d1b
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2025-11-30 16:16:07 -0500

    add btn interaction

--- main_v2.ino
+++ main_v2.ino
@@ -1,24 +1,29 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 3          // Data pin connected to the NeoPixel ring
+#define NEOPIXEL_PIN 3 // Data pin connected to the NeoPixel ring
 #define NUM_LEDS 16    // NeoPixel ring has 16 LEDs
 
-Adafruit_NeoPixel ring(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+#define BTN_PIN 6 // Data pin connected for button
+Adafruit_NeoPixel ring(NUM_LEDS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   ring.begin();
   ring.show(); // Initialize all pixels to 'off'
+  pinMode(BTN_PIN, INPUT);    // sets the digital pin 7 as input
+
 }
 
 void loop() {
-  // Turn all LEDs on (red example)
-  for (int i = 0; i < NUM_LEDS; i++) {
-    ring.setPixelColor(i, ring.Color(255, 0, 0)); // Red, change values if you want
-  }
-  ring.show();
-  delay(1000);
+  bool btn1 = digitalRead(BTN_PIN)
 
-  // Turn all LEDs off
+  if (!btn1) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      ring.setPixelColor(i, ring.Color(255, 0, 0)); 
+    }
+    ring.show();
+    delay(1000);
+  }
+  
   for (int i = 0; i < NUM_LEDS; i++) {
     ring.setPixelColor(i, ring.Color(0, 0, 0));
   }
